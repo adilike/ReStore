c899772b59c31b864e4e649e72d6509acbe4a419
adilike <adilike_francis@yahoo.com>
2022-07-02 14:30:39 +0200

    End of session 4

---
+++ client/src/features/contact/ContactPage.tsx
@@ -0,0 +1,12 @@
+import { Typography } from '@mui/material'
+import React from 'react'
+
+const ContactPage = () => {
+  return (
+    <Typography variant='h2'>
+        contactPage
+    </Typography>
+  )
+}
+
+export default ContactPage
